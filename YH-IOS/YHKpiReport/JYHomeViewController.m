403c316b57810c060381dcef60944473bf5218f8
cjg <674149625@qq.com>
2017-07-28 10:17:42 +0800

    1.实现首页扫描事件

--- YH-IOS/YHKpiReport/JYHomeViewController.m
+++ YH-IOS/YHKpiReport/JYHomeViewController.m
@@ -33,6 +33,8 @@
 #import "ManageWarningCell.h"
 #import "CommonTableViewCell.h"
 #import "BusinessGeneralCell.h"
+#import "PermissionManager.h"
+#import "SubLBXScanViewController.h"
 
 
 #define kJYNotifyHeight 40
@@ -148,7 +150,30 @@
 }
 //扫描事件
 - (void)scanAction{
-
+    [[PermissionManager shareInstance] verifyCanPhoto:^(BOOL canPhoto) {
+        LBXScanViewStyle *style = [[LBXScanViewStyle alloc]init];
+        //矩形区域中心上移，默认中心点为屏幕中心点
+        style.centerUpOffset = 44;
+        //扫码框周围4个角的类型,设置为外挂式
+        style.photoframeAngleStyle = LBXScanViewPhotoframeAngleStyle_Outer;
+        //扫码框周围4个角绘制的线条宽度
+        style.photoframeLineW = 6;
+        //扫码框周围4个角的宽度
+        style.photoframeAngleW = 24;
+        //扫码框周围4个角的高度
+        style.photoframeAngleH = 24;
+        //扫码框内 动画类型 --线条上下移动
+        style.anmiationStyle = LBXScanViewAnimationStyle_LineMove;
+        //线条上下移动图片
+        style.animationImage = [UIImage imageNamed:@"CodeScan.bundle/qrcode_scan_light_green"];
+        //SubLBXScanViewController继承自LBXScanViewController
+        //添加一些扫码或相册结果处理
+        SubLBXScanViewController *vc = [SubLBXScanViewController new];
+        vc.style = style;
+        vc.isQQSimulator = YES;
+        vc.isVideoZoom = YES;
+        [self presentViewController:vc animated:YES completion:nil];
+    }];
 }
 
 
