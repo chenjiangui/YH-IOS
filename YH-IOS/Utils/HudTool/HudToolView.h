6a7d08dc7ffa04add5ab495b231fb82e1061db2d
cjg <674149625@qq.com>
2017-07-28 14:20:57 +0800

    1.增加菊花图 类

---
+++ YH-IOS/Utils/HudTool/HudToolView.h
@@ -0,0 +1,28 @@
+//
+//  HudToolView.h
+//  YH-IOS
+//
+//  Created by cjg on 2017/7/28.
+//  Copyright © 2017年 com.intfocus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum : NSUInteger {
+    HudToolViewTypeText,
+    HudToolViewTypeLoading,
+    HudToolViewTypeTopText
+} HudToolViewType;
+
+@interface HudToolView : UIView
+
+@property (nonatomic, assign) HudToolViewType viewType;
+
+- (instancetype)initWithViewType:(HudToolViewType)viewType;
+
+
+// view nil为window
++ (void)showLoadingInView:(UIView*)view;
++ (void)hideLoadingInView:(UIView*)view;
+
+@end
