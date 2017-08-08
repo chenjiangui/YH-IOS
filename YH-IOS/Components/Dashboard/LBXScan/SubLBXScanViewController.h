8f2cbbd3e7cfb0c3b09c08625f01b7c59f3aea1a
cjg <674149625@qq.com>
2017-08-08 14:48:25 +0800

    1.修改扫一扫样式

--- YH-IOS/Components/Dashboard/LBXScan/SubLBXScanViewController.h
+++ YH-IOS/Components/Dashboard/LBXScan/SubLBXScanViewController.h
@@ -16,26 +16,26 @@
 
 @property (nonatomic, assign) BOOL isQQSimulator;
 
-/**
- @brief  扫码区域上方提示文字
- */
-@property (nonatomic, strong) UILabel *topTitle;
+///**
+// @brief  扫码区域上方提示文字
+// */
+//@property (nonatomic, strong) UILabel *topTitle;
 
 #pragma mark --增加拉近/远视频界面
 @property (nonatomic, assign) BOOL isVideoZoom;
 
-#pragma mark - 底部几个功能：开启闪光灯、相册、我的二维码
-//底部显示的功能项
-@property (nonatomic, strong) UIView *bottomItemsView;
-//相册
-@property (nonatomic, strong) UIButton *btnPhoto;
-//闪光灯
-@property (nonatomic, strong) UIButton *btnFlash;
-//我的二维码
-@property (nonatomic, strong) UIButton *btnMyQR;
-//手动输入
-@property (nonatomic, strong) UIButton *btnManualInput;
-
+//#pragma mark - 底部几个功能：开启闪光灯、相册、我的二维码
+////底部显示的功能项
+//@property (nonatomic, strong) UIView *bottomItemsView;
+////相册
+//@property (nonatomic, strong) UIButton *btnPhoto;
+////闪光灯
+//@property (nonatomic, strong) UIButton *btnFlash;
+////我的二维码
+//@property (nonatomic, strong) UIButton *btnMyQR;
+////手动输入
+//@property (nonatomic, strong) UIButton *btnManualInput;
+//
 
 
 
