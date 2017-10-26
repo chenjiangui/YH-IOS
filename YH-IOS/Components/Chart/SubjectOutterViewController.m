692a9e35ff24ac95b4744c72b9d93264f79e56fa
jericho <baofengqian@intfocus.com>
2017-10-26 15:51:44 +0800

    fixed:添加了实时报表分享由于 iOS11 布局更改造成的无法生成图片导致分享失败的问题

--- YH-IOS/Components/Chart/SubjectOutterViewController.m
+++ YH-IOS/Components/Chart/SubjectOutterViewController.m
@@ -1022,47 +1022,84 @@ static NSString *const kReportSelectorSegueIdentifier = @"ToReportSelectorSegueI
 }
 
 - (void)actionWebviewScreenShot{
-    if (self.isLoadFinish) {
-        @try {
-            UIImage *image;
-            NSString *settingsConfigPath = [FileUtils dirPath:kConfigDirName FileName:kSettingConfigFileName];
-            betaDict = [FileUtils readConfigFile:settingsConfigPath];
-            if (betaDict[@"image_within_screen"] && [betaDict[@"image_within_screen"] boolValue]) {
-                image = [self saveWebViewAsImage];
-                // image = [self createViewImage:self.navigationController.view];
-            }
-            else {
-                image = [self createViewImage:self.navigationController.view];
-            }
-            dispatch_time_t time=dispatch_time(DISPATCH_TIME_NOW, 1ull *NSEC_PER_SEC);
-            dispatch_after(time, dispatch_get_main_queue(), ^{
-                [UMSocialData defaultData].extConfig.wxMessageType = UMSocialWXMessageTypeImage;
-                [UMSocialData defaultData].extConfig.title = kWeiXinShareText;
-                [UMSocialData defaultData].extConfig.qqData.url = kBaseUrl;
-                [UMSocialSnsService presentSnsIconSheetView:self
-                                                     appKey:kUMAppId
-                                                  shareText:self.bannerName
-                                                 shareImage:image
-                                            shareToSnsNames:@[UMShareToWechatSession]
-                                                   delegate:self];
-            });
-        } @catch (NSException *exception) {
-            NSLog(@"%@", exception);
-        }
+    [_popView hideWithAnimation:NO];
+    UIImage *image;
+    NSString *settingsConfigPath = [FileUtils dirPath:kConfigDirName FileName:kSettingConfigFileName];
+    betaDict = [FileUtils readConfigFile:settingsConfigPath];
+    if (betaDict[@"image_within_screen"] && [betaDict[@"image_within_screen"] boolValue]) {
+        //image = [self captureView:self.browser frame:CGRectMake(0, 0, kScreenWidth, kScreenHeight*3)];
+        
+        image =  [self captureView:CurAppDelegate.window frame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
     }
     else {
-        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"分享提示"
-                                                                       message:@"正在加载数据，请稍后分享"
-                                                                preferredStyle:UIAlertControllerStyleAlert];
+        // image = [self captureView:self.browser frame:self.view.frame];
+        image =  [self captureView:CurAppDelegate.window frame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
         
-        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
-                                                              handler:^(UIAlertAction * action) {
-                                                                  self.isLoadFinish = self.browser.isLoading;
-                                                              }];
-        
-        [alert addAction:defaultAction];
-        [self presentViewController:alert animated:YES completion:nil];
     }
+    dispatch_time_t time=dispatch_time(DISPATCH_TIME_NOW, 1ull *NSEC_PER_SEC);
+    dispatch_after(time, dispatch_get_main_queue(), ^{
+        [UMSocialData defaultData].extConfig.wxMessageType = UMSocialWXMessageTypeImage;
+        [UMSocialData defaultData].extConfig.title = kWeiXinShareText;
+        [UMSocialData defaultData].extConfig.qqData.url = kBaseUrl;
+        [UMSocialSnsService presentSnsIconSheetView:self
+                                             appKey:kUMAppId
+                                          shareText:self.bannerName
+                                         shareImage:image
+                                    shareToSnsNames:@[UMShareToWechatSession]
+                                           delegate:self];
+    });
+}
+
+- (UIImage*)captureView:(UIView *)theView frame:(CGRect)frame
+{
+    UIGraphicsBeginImageContext(self.browser.scrollView.contentSize);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    UIImage *img;
+    if([[[UIDevice currentDevice] systemVersion] floatValue]>=7.0)
+    {
+        for(UIView *subview in theView.subviews)
+        {
+            [subview drawViewHierarchyInRect:subview.bounds afterScreenUpdates:YES];
+        }
+        img = UIGraphicsGetImageFromCurrentImageContext();
+    }
+    else
+    {
+        CGContextSaveGState(context);
+        [theView.layer renderInContext:context];
+        img = UIGraphicsGetImageFromCurrentImageContext();
+    }
+    UIGraphicsEndImageContext();
+    CGImageRef ref = CGImageCreateWithImageInRect(img.CGImage, self.browser.scrollView.frame);
+    UIImage *CGImg = [UIImage imageWithCGImage:ref];
+    CGImageRelease(ref);
+    return CGImg;
+}
+
+- (UIImage*)captureView:(UIView *)theView
+{
+    UIGraphicsBeginImageContext(theView.frame.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    UIImage *img;
+    if([[[UIDevice currentDevice] systemVersion] floatValue]>=7.0)
+    {
+        for(UIView *subview in theView.subviews)
+        {
+            [subview drawViewHierarchyInRect:subview.bounds afterScreenUpdates:YES];
+        }
+        img = UIGraphicsGetImageFromCurrentImageContext();
+    }
+    else
+    {
+        CGContextSaveGState(context);
+        [theView.layer renderInContext:context];
+        img = UIGraphicsGetImageFromCurrentImageContext();
+    }
+    UIGraphicsEndImageContext();
+    CGImageRef ref = CGImageCreateWithImageInRect(img.CGImage, theView.frame);
+    UIImage *CGImg = [UIImage imageWithCGImage:ref];
+    CGImageRelease(ref);
+    return CGImg;
 }
 
 - (UIImage *)createViewImage:(UIView *)shareView {
@@ -1072,7 +1109,6 @@ static NSString *const kReportSelectorSegueIdentifier = @"ToReportSelectorSegueI
     UIGraphicsEndImageContext();
     return image;
 }
-
 
 - (UIImage *)saveWebViewAsImage {
     UIScrollView *scrollview = self.browser.scrollView;
@@ -1093,6 +1129,7 @@ static NSString *const kReportSelectorSegueIdentifier = @"ToReportSelectorSegueI
     UIGraphicsEndImageContext();
     return image;
 }
+
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if(self.isInnerLink) {
